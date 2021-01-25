d875d127be4f5c33c62b2fea654f5d18b25c5494
lmwang27 <52217891+lmwang27@users.noreply.github.com>
2021-01-25 15:22:19 -0800

    adding javascript in html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+const name = "Eva";
+const year = new Date().getFullYear();
+
 ReactDom.render(
   <div>
-    <h1>Hello World</h1>
-    <p> This is a paragraph.</p>
+    <h1>Hello, I'm {name},My favourite food:</h1>
+    <ul>
+      <li>Vegies</li>
+      <li>Fruit</li>
+      <li>Seafood</li>
+    </ul>
+    <h1>Created getElementById @ {name} </h1>
+    <h1>Copyright @ {year} </h1>
   </div>,
   document.getElementById("root")
 );

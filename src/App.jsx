7e43254d8d2ce0943159e9e9a572239fc2abcc98
lmwang27 <52217891+lmwang27@users.noreply.github.com>
2021-01-26 13:44:47 -0800

    adjustments for components

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+import List from "./List.jsx";
+const name = "Eva";
+const year = new Date().getFullYear();
+const img = "https://picsum.photos/seed/picsum/200/300";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+      <div>
+        <img src={img + "?grayscale"} alt="" />
+      </div>
+      <p>Created getElementById @ {name} </p>
+      <p>Copyright @ {year} </p>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
-import Heading from "./Heading.jsx";
-import List from "./List.jsx";
+import App from "./App.jsx";
 
-const name = "Eva";
-const year = new Date().getFullYear();
-const img = "https://picsum.photos/seed/picsum/200/300";
-
-ReactDom.render(
-  <div>
-    <Heading />
-    <List />
-    <div>
-      <img src={img + "?grayscale"} alt="" />
-    </div>
-    <p>Created getElementById @ {name} </p>
-    <p>Copyright @ {year} </p>
-  </div>,
-  document.getElementById("root")
-);
+ReactDom.render(<App />, document.getElementById("root"));

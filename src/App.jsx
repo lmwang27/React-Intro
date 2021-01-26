b02b076b78ff713170f9e08292acde066e5a25bf
lmwang27 <52217891+lmwang27@users.noreply.github.com>
2021-01-26 14:15:04 -0800

    import  and export

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Heading from "./Heading.jsx";
 import List from "./List.jsx";
+
 const name = "Eva";
 const year = new Date().getFullYear();
 const img = "https://picsum.photos/seed/picsum/200/300";
--- src/List.jsx
+++ src/List.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PI, { doublePi, triplePi } from "./math.js";
 
 function List() {
   return (
@@ -6,6 +7,9 @@ function List() {
       <li>Bacon</li>
       <li>Banana</li>
       <li>Beef</li>
+      <li>{PI}</li>
+      <li>{doublePi()}</li>
+      <li>{triplePi()}</li>
     </ul>
   );
 }
